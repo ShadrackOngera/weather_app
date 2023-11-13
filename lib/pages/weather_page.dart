a11668f500172214bc16607450e0ae78157e969f
Shadrack Ongera <shadrack.ongera@ronforddigital.com>
2023-11-13 05:29:38 +0300

    weather page

--- lib/pages/weather_page.dart
+++ lib/pages/weather_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/models/weather_model.dart';
 import 'package:weather_app/services/weather_service.dart';
 
 class WeatherPage extends StatefulWidget {
@@ -10,11 +11,36 @@ class WeatherPage extends StatefulWidget {
 
 class _WeatherPageState extends State<WeatherPage> {
   //api Key
+
   final _weatherService = WeatherService('apiKey');
+  Weather? _weather;
+
   //fetch Weather
+  _fetchWeather() async {
+    //get current city
+
+    String cityName = await _weatherService.getCurrentCity();
+
+    //get weather for city
+
+    try {
+      final weather = await _weatherService.getWeather(cityName);
+      setState(() {
+        _weather = weather;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
 
   //animations
   @override
+  void initState() {
+    // TODO: implement initState
+    Future.delayed(Duration.zero, _fetchWeather);
+    super.initState();
+  }
+
   Widget build(BuildContext context) {
     return Scaffold();
   }
